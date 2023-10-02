598f37db816e5c3b6607956df73d67197f96f588
F-ferdous <ferdousfahim05@gmail.com>
2023-10-02 12:10:42 +0600

    Success try 2

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post("/pay-now", async (req, res) => {
     total_amount: dataBody.totalAmount,
     currency: "BDT",
     tran_id: trans_id, // use unique tran_id for each api call
-    success_url: `http://weatherdemo.idatahost.com/payment/success/${trans_id}`,
+    success_url: `weatherdemo.idatahost.com/payment/success/${trans_id}`,
     fail_url: "https://weatherdemo.idatahost.com/fail",
     cancel_url: "https://weatherdemo.idatahost.com/cancel",
     ipn_url: "http://localhost:3030/ipn",
@@ -87,7 +87,7 @@ app.post("/pay-now", async (req, res) => {
   app.post("/payment/success/:transId", async (req, res) => {
     console.log(req.params.transId);
     res.redirect(
-      `http://weatherdemo.idatahost.com/payment/success/${req.params.transId}`
+      `weatherdemo.idatahost.com/payment/success/${req.params.transId}`
     );
   });
 });
